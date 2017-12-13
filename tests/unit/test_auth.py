d1ec39c34c69f0592da5e6e6ed46e1ca641e6517
Koki Moribe <koki.moribe+coding@gmail.com>
2017-12-12 22:54:46 -0800

    init tests

---
+++ tests/unit/test_auth.py
@@ -0,0 +1,22 @@
+import pytest
+
+from moody import auth
+
+
+def test_create_token():
+    # Mock user and check token matches expected token
+    pass
+
+
+def get_token_from_request():
+    # Monkey patch flask request and test if correct exceptions are raised for invalid tokens
+    pass
+
+
+def decode_token():
+    # Test if valid tokens return expected payload
+    # Test if invalid tokens raise exception
+    pass
+
+
+
