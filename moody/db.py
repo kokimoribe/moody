4a4f2e2cb97310e27c1b48879963c26fb8bb3ba3
Koki Moribe <koki.moribe+coding@gmail.com>
2017-12-12 22:52:13 -0800

    init db

---
+++ moody/db.py
@@ -0,0 +1,49 @@
+"""Database"""
+import time
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.exc import OperationalError
+
+from moody.config import DATABASE_URL, DEBUG
+
+engine = create_engine(DATABASE_URL, echo=DEBUG)
+
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+
+def create_tables():
+    """Create tables"""
+    from moody.models import Base
+    test_connection()
+    Base.metadata.create_all(bind=engine)
+
+
+def drop_tables():
+    """Drop tables"""
+    from moody.models import Base
+    test_connection()
+    Base.metadata.drop_all(bind=engine)
+
+
+def test_connection(max_attempts=5, interval_secs=1.0):
+    """
+    Test connection to database.
+
+    If connection fails, retry after `interval_secs` for a total of `max_attempts`.
+    """
+    num_attempts = 1
+    while True:
+        try:
+            with engine.connect():
+                return
+
+        except OperationalError:
+            if num_attempts > max_attempts:
+                msg = 'Connecting to database failed after {} attempts'.format(
+                    max_attempts)
+                print(msg)
+                raise
+            # Retry if connection refused
+            num_attempts += 1
+            time.sleep(interval_secs)
